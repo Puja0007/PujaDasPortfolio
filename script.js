6b49d3d998c7cba8756d2d549c2b64fec312600c
puja <puja.das@geotechinfo.net>
2021-09-04 19:10:28 +0530

    final commit

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer"],
+        strings: ["Frontend Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
